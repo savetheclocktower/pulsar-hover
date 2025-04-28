e93c68b310d284a098a2ea143d277ed5f07226a0
Andrew Dupont <github@andrewdupont.net>
2025-04-28 15:09:06 -0700

    Commit built files

--- lib/render-markdown.d.ts
+++ lib/render-markdown.d.ts
@@ -2,14 +2,23 @@ import { Grammar } from 'atom';
 export type DOMPurifyConfig = Omit<DOMPurify.Config, "RETURN_DOM" | "RETURN_DOM_FRAGMENT" | "RETURN_TRUSTED_TYPE"> & {
     PARSER_MEDIA_TYPE: DOMParserSupportedType | null;
 };
+/**
+ * All the properties in `T` whose values are strings.
+ */
 type StringPropertyNames<T> = {
     [K in keyof T]: T[K] extends string ? K : never;
 }[keyof T];
+/**
+ * All the properties in `T` whose values aren't readonly.
+ */
 type WritablePropertyNames<T> = {
     [K in keyof T]: T extends {
         readonly [P in K]: T[K];
     } ? never : K;
 }[keyof T];
+/**
+ * All the writable string properties in `CSSStyleDeclaration`.
+ */
 type WritableStringCSSProperties = Pick<CSSStyleDeclaration, StringPropertyNames<CSSStyleDeclaration> & WritablePropertyNames<CSSStyleDeclaration>>;
 export type RenderMarkdownFragmentOptions = {
     markdown: string;
